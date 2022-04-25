9c8e631eaca68e8c7da1cc39a881a4d0f1db7566
Colomboo1 <andreslatorretottis@gmail.com>
2022-04-25 20:51:59 -0300

    1 file modified

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,22 +1,22 @@
-import React,{useState, useEffect} from "react";
+import React,{useState, useEffect,useContext} from "react";
 import {CartContext}  from '../context/StoreProvider';
 import { GoDiffAdded,GoDiffRemoved } from "react-icons/go";
 
 
 const ItemCount = ({id,stock,price}) => {
   
-  const [count, setCount] = useState(0);
+  const [quantity, setQuantity] = useState(0);
   
-  const [buy, setBuy] = useState({});
-  //const {CartContext} = CartContext;
+  
+  const [cartArray,setCartArray] = useContext(CartContext)
   
   const addstock = () => {
-    setCount(count +1);
+    setQuantity(quantity +1);
   
   };
   
   const remStock = () => {
-    setCount(count -1)
+    setQuantity(quantity -1)
   };
   {/* 
 
@@ -24,28 +24,48 @@ const ItemCount = ({id,stock,price}) => {
 
 
     esta bosta estaba reconociendo la funcion y mandaba el console log con los datos para mandar al context pero no me permite mandar el objetos con los datos
+
+if 
+
+
   */}
   
-  const purchasedLoad = () =>{
-    setBuy({id,count,price})
-    console.log("entro en funcion",id,count,price);
+  const loaderCart = () =>{
+    
+    const newArray = [
+      ...cartArray.list,
+      {id, quantity, price}
+    ]
+    const cartModel={
+      list: newArray,
+      count:0,
+      mount:0
+    };
+    
+    setCartArray(cartModel)  
+    console.log("newArray",cartModel);
   }
-  useEffect(() => {
-   // CartContext = setCartItemModel = (...[id, count, price]);
-    console.log("carga de datos" ,id,count,price)
-  }, [{purchasedLoad}]);
+
+
+
+
+  {/*
+    useEffect(() => {
+      console.log("carga de datos" ,id,quantity,price)
+    }, [{purchasedLoad}]);
+  */}
 
   
   
   return (
-    <div className="count">
+    <div className="quantity">
       <div className="stock">
-        <button className="btn-rem" onClick={remStock} disabled={ count<= 0 ? true : null}><GoDiffRemoved size="3em"/></button>
+        <button className="btn-rem" onClick={remStock} disabled={ quantity === 0} ><GoDiffRemoved size="3em"/></button>
         <div>
-          <p>cantidad: {count}</p>
-          <button className="btn-buy" onClick={purchasedLoad} disabled={ count<= 0 ? true : null} >agregar al carrito</button>
+          <p>cantidad: {quantity}</p>
+          <button className="btn-buy" onClick={loaderCart} disabled={ stock === 0 || quantity  === 0 } >agregar al carrito</button>
         </div>
-        <button className="btn-add" onClick={addstock} disabled={ count>= stock ? true : null}><GoDiffAdded size="3em"/></button>
+        <button className="btn-add" onClick={addstock} disabled={ quantity === stock }><GoDiffAdded size="3em"/></button>
       </div>
     </div>  
   );
